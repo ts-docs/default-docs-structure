cabf8bd102de7b826566c32edc66a88e25b83758
GoogleFeud <volen.sl666@gmail.com>
2021-08-01 16:45:26 +0300

    setup

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const config = {
+    mode: "production",
+    entry: path.join(__dirname, "src/assets/js/index.ts"),
+
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+
+    output: {
+        filename: "index.js",
+        path: path.join(__dirname, "dist/assets/js")
+    },
+
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.join(__dirname, "src/assets/css"),
+                    to: path.join(__dirname, "dist/assets/css"),
+                },
+                {
+                    from: path.join(__dirname, "src/assets/media"),
+                    to: path.join(__dirname, "dist/assets/media"),
+                },
+                {
+                    from: path.join(__dirname, "src/partials"),
+                    to: path.join(__dirname, "dist/partials")
+                },
+                {
+                    from: path.join(__dirname, "src/index.hbs"),
+                    to: path.join(__dirname, "dist/index.hbs")
+                },
+                {
+                    from: path.join(__dirname, "src/components"),
+                    to: path.join(__dirname, "dist/components")
+                },
+                {
+                    from: path.join(__dirname, "src/helpers.js"),
+                    to: path.join(__dirname, "dist/helpers.js")
+                }
+            ],
+        }),
+    ],
+};
+
+module.exports = config;
