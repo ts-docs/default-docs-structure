025b796e4e13f0b0b52a7eda08cfe08ed3e18664
GoogleFeud <volen.sl666@gmail.com>
2021-08-07 17:47:51 +0300

    Functions and constants

--- src/helpers.js
+++ src/helpers.js
@@ -20,17 +20,19 @@ Handlebars.registerHelper("resolveSidebar", (ctx) => {
         const nested = data.isNested ? ".":"";
         return `
             <p>Modules</p>
-            <ul>${[...data.module.modules.values()].map(c => `<li><a href="${nested}./${c.name}/index.html">${c.name}</a></li>`).join("")}</ul>
+            <ul>${[...data.module.modules.values()].map(c => `<li><a href="${nested}./m.${c.name}/index.html">${c.name}</a></li>`).join("")}</ul>
             <p>Classes</p>
             <ul>${[...data.module.classes.values()].map(c => `<li><a href="${nested}./class/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
             <p>Interfaces</p>
             <ul>${[...data.module.interfaces.values()].map(c => `<li><a href="${nested}./interface/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
             <p>Functions</p>
-            <ul>${[...data.module.functions.values()].map(c => `<li><a href="${nested}./interface/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
+            <ul>${[...data.module.functions.values()].map(c => `<li><a href="${nested}./function/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
             <p>Enums</p>
             <ul>${[...data.module.enums.values()].map(c => `<li><a href="${nested}./enum/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
             <p>Types</p>
             <ul>${[...data.module.types.values()].map(c => `<li><a href="${nested}./type/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
+            <p>Constants</p>
+            <ul>${data.module.constants.map(c => `<li><a href="${nested}./constant/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
         `
     } else if (data.type === "interface") {
         return `
@@ -45,7 +47,7 @@ Handlebars.registerHelper("resolveSidebar", (ctx) => {
     } else if (data.type === "index") {
         return `
             <p>Modules</p>
-            <ul>${data.packages.map(c => `<li><a href="./${c.module.name}/index.html">${c.module.name}</a></li>`).join("")}</ul>
+            <ul>${data.packages.map(c => `<li><a href="./m.${c.module.name}/index.html">${c.module.name}</a></li>`).join("")}</ul>
         `
     }
 });
