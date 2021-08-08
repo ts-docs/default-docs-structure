de3db8f13212a8c9f87c6c03b8bb96aa2d93793b
GoogleFeud <volen.sl666@gmail.com>
2021-08-08 16:23:36 +0300

    custom pages

--- src/helpers.js
+++ src/helpers.js
@@ -17,22 +17,23 @@ Handlebars.registerHelper("resolveSidebar", (ctx) => {
         </ul>
         `
     } else if (data.type === "module") {
-        const nested = data.isNested ? ".":"";
+        const depth = "../".repeat(data.depth);
         return `
+            ${data.pages ? data.pages.map(cat => `<p>${cat.name}</p><ul>${cat.pages.map(p => `<li><a href="${depth}pages/${cat.name}/${p.name}.html">${p.name}</a></li>`)}</ul>`).join(""):""}
             <p>Modules</p>
-            <ul>${[...data.module.modules.values()].map(c => `<li><a href="${nested}./m.${c.name}/index.html">${c.name}</a></li>`).join("")}</ul>
+            <ul>${[...data.module.modules.values()].map(c => `<li><a href="${depth}m.${c.name}/index.html">${c.name}</a></li>`).join("")}</ul>
             <p>Classes</p>
-            <ul>${[...data.module.classes.values()].map(c => `<li><a href="${nested}./class/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
+            <ul>${[...data.module.classes.values()].map(c => `<li><a href="${depth}class/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
             <p>Interfaces</p>
-            <ul>${[...data.module.interfaces.values()].map(c => `<li><a href="${nested}./interface/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
+            <ul>${[...data.module.interfaces.values()].map(c => `<li><a href="${depth}interface/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
             <p>Functions</p>
-            <ul>${[...data.module.functions.values()].map(c => `<li><a href="${nested}./function/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
+            <ul>${[...data.module.functions.values()].map(c => `<li><a href="${depth}function/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
             <p>Enums</p>
-            <ul>${[...data.module.enums.values()].map(c => `<li><a href="${nested}./enum/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
+            <ul>${[...data.module.enums.values()].map(c => `<li><a href="${depth}enum/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
             <p>Types</p>
-            <ul>${[...data.module.types.values()].map(c => `<li><a href="${nested}./type/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
+            <ul>${[...data.module.types.values()].map(c => `<li><a href="${depth}type/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
             <p>Constants</p>
-            <ul>${data.module.constants.map(c => `<li><a href="${nested}./constant/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
+            <ul>${data.module.constants.map(c => `<li><a href="${depth}constant/${c.name}.html">${c.name}</a></li>`).join("")}</ul>
         `
     } else if (data.type === "interface") {
         return `
@@ -45,9 +46,11 @@ Handlebars.registerHelper("resolveSidebar", (ctx) => {
             <ul>${data.members.map(m => `<li><a href="#${m.name}">${m.name}</a></li>`).join("")}</ul>
         `;
     } else if (data.type === "index") {
+        const depth = "../".repeat(data.depth);
         return `
+            ${data.pages ? data.pages.map(cat => `<p>${cat.name}</p><ul>${cat.pages.map(p => `<li><a href="${depth}pages/${cat.name}/${p.name}.html">${p.name}</a></li>`)}</ul>`).join(""):""}
             <p>Modules</p>
-            <ul>${data.packages.map(c => `<li><a href="./m.${c.module.name}/index.html">${c.module.name}</a></li>`).join("")}</ul>
+            <ul>${data.packages.map(c => `<li><a href="${depth}m.${c.module.name}/index.html">${c.module.name}</a></li>`).join("")}</ul>
         `
     }
 });
