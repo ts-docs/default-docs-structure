8bd67acf40e9d87ffb08308569a698dd5482c864
GoogleFeud <volen.sl666@gmail.com>
2022-03-11 20:26:09 +0200

    Smaller sidebar categories and more organized constants

--- src/components/constant.tsx
+++ src/components/constant.tsx
@@ -1,21 +1,26 @@
 
-import type { ConstantDecl } from "@ts-docs/extractor";
+import { ConstantDecl, TypeKinds } from "@ts-docs/extractor";
 import type { Generator } from "@ts-docs/ts-docs";
 
 export function render(gen: Generator, { constant, content }: {
     constant: ConstantDecl,
     content?: string
 }) {
-    const [blockComment, inlineComment] = gen.generateComment(constant.jsDoc, true) || ["", ""]
+    const [blockComment, inlineComment] = gen.generateComment(constant.jsDoc, true) || ["", ""];
+    const type = constant.type && constant.type.kind !== TypeKinds.STRINGIFIED_UNKNOWN && gen.generateType(constant.type);
     return <div>
         <h1>Const <span class="referenceLink constant">{constant.name}</span>{inlineComment}</h1>
         {constant.loc.sourceFile ? <p><a class="secondary-text" href={constant.loc.sourceFile}>Defined in {constant.loc.filename}</a></p> : ""}
 
-        {constant.type ? gen.generateType(constant.type) : ""}
+        {type ? <div>
+            <h2>Type</h2>
+            {type}    
+        </div> : ""}
 
         {blockComment}
 
         {content ? <div>
+            <h2>Content</h2>
 <pre>
 <code class="hljs">
 {content}
